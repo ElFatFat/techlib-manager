4605c3271a0fdd968a388ca539f79204f97602c2
Valentin T <92257425+ElFatFat@users.noreply.github.com>
2024-12-13 13:15:05 +0100

    First Deploy

---
+++ models/Livre.js
@@ -0,0 +1,36 @@
+// models/Livre.js
+const mongoose = require('mongoose');
+
+const LivreSchema = new mongoose.Schema({
+    titre: { 
+        type: String, 
+        required: true 
+    },
+    auteurs: { 
+        type: [String], 
+        required: true, 
+    },
+    ISBN: { 
+        type: String, 
+        required: true,
+    },
+    categorie: {
+        type: String,
+        enum: ['Programming' , 'DevOps' , 'Design' , 'Other'],
+        required: true,
+    },
+    nombrePages: {
+        type: Number,
+        required: true,
+    },
+    statutLecture: {
+        type: String,
+        enum: ['A lire' , 'En cours' , 'Terminé'],
+        required: true,
+    },
+    notesPersonnelles: {
+        type: [String],
+    },
+});
+
+module.exports = mongoose.model('Livre', LivreSchema);
---
+++ routes/books.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var router = express.Router();
+var Livre = require('../models/Livre');
+
+// ✨ Création : Formulaire d'ajout d'un nouveau livre
+router.get('/new', function(req, res, next) {
+  res.render('addBook', { title: 'Ajouter un nouveau livre' });
+});
+
+router.post('/new', async function(req, res, next) {
+  try {
+    const newLivre = new Livre(req.body);
+    await newLivre.save();
+    res.redirect('/books');
+  } catch (error) {
+    next(error);
+  }
+});
+
+// 👀 Lecture : Liste paginée de tous les livres
+router.get('/', async function(req, res, next) {
+  try {
+    const livres = await Livre.find();
+    res.render('bookList', { title: 'Liste des livres', livres });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// 👀 Lecture : Vue détaillée d'un livre
+router.get('/:id', async function(req, res, next) {
+  try {
+    const livre = await Livre.findById(req.params.id);
+    res.render('bookDetail', { title: 'Détails du livre', livre });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// ✏️ Modification : Formulaire d'édition d'un livre existant
+router.get('/:id/edit', async function(req, res, next) {
+  try {
+    const livre = await Livre.findById(req.params.id);
+    res.render('editBook', { title: 'Modifier le livre', livre });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/:id/edit', async function(req, res, next) {
+  try {
+    await Livre.findByIdAndUpdate(req.params.id, req.body);
+    res.redirect('/books/' + req.params.id);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// 🗑️ Suppression : Possibilité de retirer un livre de la liste
+router.post('/:id/delete', async function(req, res, next) {
+  try {
+    await Livre.findByIdAndDelete(req.params.id);
+    res.redirect('/books');
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
